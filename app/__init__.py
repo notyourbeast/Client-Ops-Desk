9d8ef5e79d7d970fa72e6d4798578250ac38531d
Saiprasad Shiragave <saishiragave@gmail.com>
2025-11-21 10:54:50 +0000

    added comfiguration and env loading(ref: python-dotenv)

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+from .config import Config
+
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
-    app.config.from_object('app.config.Config')
+    app.config.from_object(Config)
 
     @app.route('/')
     def index():
--- app/config.py
+++ app/config.py
@@ -1,6 +1,12 @@
 import os
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 class Config:
     SECRET_KEY = os.environ.get('FCH_SECRET_KEY', 'dev-secret')
     MONGO_URI = os.environ.get('FCH_MONGO_URI', '')
+    JWT_SECRET_KEY = os.environ.get('FCH_JWT_SECRET_KEY', 'dev-jwt-secret')
